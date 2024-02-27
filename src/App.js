81b61de67bf7fbe245702213a5e9ed58e072d2fd
luqmanul0701 <naufalluqmanul29@gmail.com>
2024-02-27 22:18:45 +0800

    aplikasi list barang

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [activities, setActivities] = useState([]);
+
+  const addActivity = (activity) => {
+    setActivities([...activities, activity]);
+  };
+
+  const markActivityComplete = (id) => {
+    const updatedActivities = activities.map((activity) => (activity.id === id ? { ...activity, completed: true } : activity));
+    setActivities(updatedActivities);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List Barang Belanja</h1>
+      <ActivityForm onAddActivity={addActivity} />
+      <ActivityList activities={activities} onActivityComplete={markActivityComplete} />
     </div>
   );
 }
 
-export default App;
+function ActivityForm({ onAddActivity }) {
+  const [activity, setActivity] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!activity) return;
+    onAddActivity({ id: Date.now(), description: activity, completed: false });
+    setActivity("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Masukan Barang Anda" value={activity} onChange={(e) => setActivity(e.target.value)} />
+      <button className="button1" type="submit">
+        Tambahkan List
+      </button>
+    </form>
+  );
+}
+
+function ActivityList({ activities, onActivityComplete }) {
+  return (
+    <ul>
+      {activities.map((activity) => (
+        <Activity key={activity.id} activity={activity} onActivityComplete={onActivityComplete} />
+      ))}
+    </ul>
+  );
+}
+
+function Activity({ activity, onActivityComplete }) {
+  const { id, description, completed } = activity;
+
+  return (
+    <li style={{ textDecoration: completed ? "line-through" : "none" }}>
+      {description}
+      {!completed && <button onClick={() => onActivityComplete(id)}>Done mas😬</button>}
+    </li>
+  );
+}
